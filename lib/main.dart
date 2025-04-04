2b82bd2274d79084622661c115eb4d8a70855642
Md. Hanan Sajid <mhs@mbp16.local>
2025-04-04 22:43:57 +0530

    Mainscreen to handle navigation

--- lib/main.dart
+++ lib/main.dart
@@ -108,3 +108,60 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
+
+//---------------------MainScreen to handle navigation -----------------------------
+class MainScreen extends StatefulWidget {
+  final WalletService walletService;
+  MainScreen({required this.walletService});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  late List<Widget> _screens;
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      HomeContent(),
+      WalletScreen(walletService: widget.walletService),
+      SettingsScreen(),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.deepPurple[500],
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
